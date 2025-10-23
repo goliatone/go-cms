a2e89b66c0e3b9433bc5bdf7a3c5700d7b7291a6
goliatone <140582+goliatone@users.noreply.github.com>
2025-10-23 09:09:58 -0700

    add: cache implementation

--- config.go
+++ config.go
@@ -1,11 +1,14 @@
 package cms
 
+import "time"
+
 type Config struct {
 	Enabled       bool
 	DefaultLocale string
 	Content       ContentConfig
 	I18N          I18NConfig
 	Storage       StorageConfig
+	Cache         CacheConfig
 }
 
 type ContentConfig struct {
@@ -21,6 +24,11 @@ type StorageConfig struct {
 	Provider string
 }
 
+type CacheConfig struct {
+	Enabled    bool
+	DefaultTTL time.Duration
+}
+
 func DefaultConfig() Config {
 	return Config{
 		Enabled:       true,
@@ -34,6 +42,10 @@ func DefaultConfig() Config {
 		},
 		Storage: StorageConfig{
 			Provider: "bun",
+		},
+		Cache: CacheConfig{
+			Enabled:    true,
+			DefaultTTL: time.Minute,
 		},
 	}
 }
