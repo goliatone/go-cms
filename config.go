7fa70a4d233cf5b1321f2262a42d43a864d9b317
goliatone <140582+goliatone@users.noreply.github.com>
2025-10-31 13:48:46 -0700

    fix: espose widget config

--- config.go
+++ config.go
@@ -14,20 +14,22 @@ var (
 )
 
 type (
-	Config               = runtimeconfig.Config
-	ContentConfig        = runtimeconfig.ContentConfig
-	I18NConfig           = runtimeconfig.I18NConfig
-	StorageConfig        = runtimeconfig.StorageConfig
-	CacheConfig          = runtimeconfig.CacheConfig
-	NavigationConfig     = runtimeconfig.NavigationConfig
-	ThemeConfig          = runtimeconfig.ThemeConfig
-	URLKitResolverConfig = runtimeconfig.URLKitResolverConfig
-	Features             = runtimeconfig.Features
-	CommandsConfig       = runtimeconfig.CommandsConfig
-	MarkdownConfig       = runtimeconfig.MarkdownConfig
-	MarkdownParserConfig = runtimeconfig.MarkdownParserConfig
-	GeneratorConfig      = runtimeconfig.GeneratorConfig
-	LoggingConfig        = runtimeconfig.LoggingConfig
+	Config                 = runtimeconfig.Config
+	ContentConfig          = runtimeconfig.ContentConfig
+	I18NConfig             = runtimeconfig.I18NConfig
+	StorageConfig          = runtimeconfig.StorageConfig
+	CacheConfig            = runtimeconfig.CacheConfig
+	NavigationConfig       = runtimeconfig.NavigationConfig
+	ThemeConfig            = runtimeconfig.ThemeConfig
+	URLKitResolverConfig   = runtimeconfig.URLKitResolverConfig
+	WidgetConfig           = runtimeconfig.WidgetConfig
+	WidgetDefinitionConfig = runtimeconfig.WidgetDefinitionConfig
+	Features               = runtimeconfig.Features
+	CommandsConfig         = runtimeconfig.CommandsConfig
+	MarkdownConfig         = runtimeconfig.MarkdownConfig
+	MarkdownParserConfig   = runtimeconfig.MarkdownParserConfig
+	GeneratorConfig        = runtimeconfig.GeneratorConfig
+	LoggingConfig          = runtimeconfig.LoggingConfig
 )
 
 func DefaultConfig() Config {
